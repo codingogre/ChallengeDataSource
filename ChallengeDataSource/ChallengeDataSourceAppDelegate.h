3589cef2956478bca24eee46f8f9bcb8e05e2e79
Shawn Hooton <shooton@gopivotal.com>
2014-02-09 14:34:31 -0500

    Initial Commit

---
+++ ChallengeDataSource/ChallengeDataSourceAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ChallengeDataSourceAppDelegate.h
+//  ChallengeDataSource
+//
+//  Created by Shawn Hooton on 2/9/14.
+//  Copyright (c) 2014 Sixpanel LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ChallengeDataSourceAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ChallengeDataSource/ChallengeDataSourceAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  ChallengeDataSourceAppDelegate.m
+//  ChallengeDataSource
+//
+//  Created by Shawn Hooton on 2/9/14.
+//  Copyright (c) 2014 Sixpanel LLC. All rights reserved.
+//
+
+#import "ChallengeDataSourceAppDelegate.h"
+
+@implementation ChallengeDataSourceAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
